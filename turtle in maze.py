d2d2f9ad1dbbfbdeb7ec72870b2deb83cac77fff
zlatatikhomirova <72922674+zlatatikhomirova@users.noreply.github.com>
2021-11-18 18:09:26 +0700

    Correct function
    Done!

--- turtle in maze.py
+++ turtle in maze.py
@@ -1,23 +1,6 @@
 import turtle 
-import sys
 
-def main():
-    wn = turtle.Screen()              
-    wn.bgcolor("white")               
-    wn.setup(1300,700) 
-
-    maze = turtle.Turtle()
-    maze.shape("square")           
-    maze.color("black")
-    maze.penup()                   
-    maze.speed(3)
-
-    joe = turtle.Turtle()
-    joe.color("blue")
-    joe.speed(3)
-    joe.penup() 
-    
-    def can_exit(grid: list) -> bool:
+def can_exit(grid: list) -> bool:
         queue = [(0, 0)] 
         visited = {} 
         visited[(0, 0)] = (-1, -1)
@@ -26,39 +9,75 @@ def main():
         while len(queue) > 0: 
             r, c = queue.pop(0)
             if (r, c) == ends:
-                joe.goto(-650 + 24 + c * 24, 350 - 24 - r * 24)
                 return True
             for dx, dy in ((-1, 0), (0, -1), (1, 0), (0, 1)):
                 new_r = r + dy
                 new_c = c + dx
                 if (-1 < new_r < end_r) and (-1 < new_c < end_c) and not ((new_r, new_c) in visited or grid[new_r][new_c]):
                     visited[(new_r, new_c)] = (r, c)
-                    joe.goto(-650 + 24 + c * 24, 350 - 24 - r * 24)
                     queue.append((new_r, new_c))
         return False
+    
+def draw_maze(grid: list):
+    wn = turtle.Screen()              
+    wn.bgcolor("black")               
+    wn.setup(1300,700) 
 
-    def setup_maze(grid: list):
-        for y in range(len(grid)):                       
-            for x in range(len(grid[0])):                
-                character = grid[y][x]                   
-                screen_x = -650 + 24 + x * 24          
-                screen_y = 350 - 24 - y * 24       
+    maze = turtle.Turtle()
+    maze.shape("square")           
+    maze.penup()                   
+    maze.speed(3)
 
-                if character == 1:                     
-                    maze.goto(screen_x, screen_y)        
-                    maze.stamp()                         
+    joe = turtle.Turtle()
+    joe.color("blue")
+    joe.shape("circle")
+    joe.speed(2)
+    joe.penup() 
+    
+    for y in range(len(grid)):                       
+        for x in range(len(grid[0])):                
+            character = grid[y][x]                   
+            screen_x = -650 + 24 + x * 24          
+            screen_y = 350 - 24 - y * 24       
+
+            if character == 1:     
+                maze.color("green")                
+                maze.goto(screen_x, screen_y)        
+                maze.stamp()
+            else:
+                maze.goto(screen_x, screen_y) 
+                maze.color("yellow")
+                maze.stamp()                        
 
 
-                if (x, y) == (0, 0):                     
-                    joe.goto(screen_x, screen_y)     
+            if (x, y) == (0, 0):                     
+                joe.goto(screen_x, screen_y)
+                
+    
+    queue = [(0, 0)] 
+    visited = {} 
+    visited[(0, 0)] = (-1, -1)
+    end_r, end_c = len(grid), len(grid[0])
+    ends = (end_r-1, end_c-1)
+    while len(queue) > 0: 
+        r, c = queue.pop(0)
+        if (r, c) == ends:
+            joe.goto(-650 + 24 + c * 24, 350 - 24 - r * 24)
+        for dx, dy in ((-1, 0), (0, -1), (1, 0), (0, 1)):
+            new_r = r + dy
+            new_c = c + dx
+            if (-1 < new_r < end_r) and (-1 < new_c < end_c) and not ((new_r, new_c) in visited or grid[new_r][new_c]):
+                visited[(new_r, new_c)] = (r, c)
+                joe.goto(-650 + 24 + c * 24, 350 - 24 - r * 24)
+                queue.append((new_r, new_c))  
 
 
-setup_maze([
+draw_maze([
          [0, 1, 1, 1, 1, 1, 1],
          [0, 0, 1, 1, 0, 1, 1],
          [1, 0, 0, 0, 0, 1, 1],
-         [1, 1, 1, 1, 0, 0, 1],
-         [1, 1, 1, 1, 1, 0, 0]
+         [1, 1, 0, 1, 0, 0, 1],
+         [1, 0, 0, 1, 1, 0, 0]
         ])
 
 assert can_exit([
@@ -68,14 +87,6 @@ assert can_exit([
                  [1, 1, 1, 1, 0, 0, 1],
                  [1, 1, 1, 1, 1, 0, 0]
                 ]) == True, "Test 1. WA."
-
-setup_maze([
-             [0, 1, 1, 1, 1, 1, 1],
-             [0, 0, 1, 0, 0, 1, 1],
-             [1, 0, 0, 0, 0, 1, 1],
-             [1, 1, 0, 1, 0, 0, 1],
-             [1, 1, 0, 0, 1, 1, 1]
-           ])
  
 assert can_exit([
                  [0, 1, 1, 1, 1, 1, 1],
@@ -85,14 +96,6 @@ assert can_exit([
                  [1, 1, 0, 0, 1, 1, 1]
                ]) == False, "Test 2. WA."
 
-setup_maze([
-             [0, 1, 1, 1, 1, 0, 0],
-             [0, 0, 0, 0, 1, 0, 0],
-             [1, 1, 1, 0, 0, 0, 0],
-             [1, 1, 1, 1, 1, 1, 0],
-             [1, 1, 1, 1, 1, 1, 1]
-            ])
-
 assert can_exit([
                  [0, 1, 1, 1, 1, 0, 0],
                  [0, 0, 0, 0, 1, 0, 0],
@@ -100,14 +103,6 @@ assert can_exit([
                  [1, 1, 1, 1, 1, 1, 0],
                  [1, 1, 1, 1, 1, 1, 1]
                 ]) == False, "Test 3. WA."
-
-setup_maze([
-             [0, 1, 1, 1, 1, 0, 0],
-             [0, 0, 0, 0, 1, 0, 0],
-             [1, 1, 1, 0, 0, 0, 0],
-             [1, 0, 0, 0, 1, 1, 0],
-             [1, 1, 1, 1, 1, 1, 0]
-            ])
  
 assert can_exit([
                  [0, 1, 1, 1, 1, 0, 0],
@@ -116,6 +111,3 @@ assert can_exit([
                  [1, 0, 0, 0, 1, 1, 0],
                  [1, 1, 1, 1, 1, 1, 0]
                 ]) == True, "Test 4. WA."
-
-wn.exitonclick()
-sys.exit()
